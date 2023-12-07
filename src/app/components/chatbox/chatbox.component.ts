df963e69940df89781c20778198f0250ac736f49
Roman Tereshchenko <16310701+romandott@users.noreply.github.com>
2023-12-07 19:37:54 +0200

    feat: add close link

--- src/app/components/chatbox/chatbox.component.ts
+++ src/app/components/chatbox/chatbox.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, ActivatedRouteSnapshot, RouterLink, RouterModule } from '@angular/router';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'virtual-interviewer-chatbox',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterModule],
   templateUrl: './chatbox.component.html',
   styleUrl: './chatbox.component.scss',
 })
-export class ChatboxComponent {}
+export class ChatboxComponent {
+  public readonly vacancyId$: Observable<string | null> = this.getVacancyId()
+
+  public constructor(public readonly route: ActivatedRoute) {
+    console.log(route)
+  }
+
+  private getVacancyId(): Observable<string | null> {
+    return this.route.paramMap.pipe(map(params => params.get('id')))
+  }
+}
